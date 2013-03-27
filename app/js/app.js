cee564ea62d99d4e32e7f5531826269106b4525b
emarinizquierdo <e.marin.izquierdo@gmail.com>
2013-03-27 08:30:20 +0100

    update project

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'flickrCarousel']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'flickrCarousel', 'HashBangURLs']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: OnNavigate });
     $routeProvider.when('/demos/01/demo/01', {templateUrl: 'partials/demo01.html', controller: OnNavigate });
@@ -9,3 +9,12 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/demos/01/demo/04', {templateUrl: 'partials/demo04.html', controller: OnNavigate });
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
+
+angular.module('HashBangURLs', []).config(['$locationProvider', function($location) {
+  $location.hashPrefix('!');
+}]);
+
+//get the module from creating an angular module
+angular.module('HTML5ModeURLs', []).config(['$routeProvider', function($route) {
+  $route.html5Mode(true);
+}]);
